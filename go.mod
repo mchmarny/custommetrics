e03caf562158349f91aee89993f20beba02b2ab8
Mark Chmarny <mark@chmarny.com>
2019-05-13 20:55:27 -0700

    initial take on dynamic json queries

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	cloud.google.com/go v0.39.0
 	github.com/golang/protobuf v1.3.1
 	google.golang.org/genproto v0.0.0-20190513181449-d00d292a067c
+	gopkg.in/thedevsaddam/gojsonq.v2 v2.2.1
 )
--- processor.go
+++ processor.go
@@ -2,7 +2,7 @@ package custommetrics
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -13,15 +13,17 @@ import (
 	metricpb "google.golang.org/genproto/googleapis/api/metric"
 	monitoredrespb "google.golang.org/genproto/googleapis/api/monitoredres"
 	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
-)
 
-const (
-	customMetricType = "custom.googleapis.com/partner/mocked"
+	"gopkg.in/thedevsaddam/gojsonq.v2"
 )
 
 var (
-	logger    = log.New(os.Stdout, "[METRICS] ", 0)
-	projectID = mustEnvVar("PID", "")
+	logger = log.New(os.Stdout, "[METRICS] ", 0)
+
+	projectID       = mustEnvVar("PID", "")
+	metricType      = mustEnvVar("METRIC_TYPE", "custom.googleapis.com/metric/default")
+	metricSrcIDPath = mustEnvVar("METRIC_SRC_ID_PATH", "")
+	metricValuePath = mustEnvVar("METRIC_VALUE_PATH", "")
 
 	once          sync.Once
 	monitorClient *monitoring.MetricClient
@@ -30,20 +32,6 @@ var (
 // PubSubMessage is the payload of a Pub/Sub event
 type PubSubMessage struct {
 	Data []byte `json:"data"`
-}
-
-// TextContent represents generic text event
-type TextContent struct {
-	Symbol    string    `json:"symbol"`
-	ID        string    `json:"cid"`
-	CreatedAt time.Time `json:"created"`
-	Author    string    `json:"author"`
-	Lang      string    `json:"lang"`
-	Source    string    `json:"source"`
-	Content   string    `json:"content"`
-	Magnitude float32   `json:"magnitude"`
-	Score     float32   `json:"score"`
-	IsRetweet bool      `json:"retweet"`
 }
 
 // ProcessorSentiment processes pubsub topic events
@@ -59,44 +47,54 @@ func ProcessorSentiment(ctx context.Context, m PubSubMessage) error {
 		monitorClient = mc
 	})
 
-	var c TextContent
-	if err := json.Unmarshal(m.Data, &c); err != nil {
-		logger.Fatalf("Error converting data: %v", err)
-	}
+	json := string(m.Data)
+	logger.Printf("JSON: %s", json)
 
-	content, err := json.Marshal(c)
-	if err != nil {
-		logger.Fatalf("Error marshaling content: %v", err)
-	}
+	jq := gojsonq.New().JSONString(json)
 
-	logger.Printf("Payload: %v", content)
+	metricSrcID := jq.Find(metricSrcIDPath)
+	logger.Printf("METRIC_SRC_ID: %v", metricSrcID)
 
-	//extract metrics and send them to publishMetric
-	publishMetric(ctx, "123", 1.2345)
+	metricValue := jq.Find(metricValuePath)
+	logger.Printf("METRIC_VALUE: %v", metricValue)
 
-	return err
+	//TODO: implement value conversion extract metrics and send them to publishMetric
+	return publishMetric(ctx, "123", 1.2345)
 
 }
 
-func publishMetric(ctx context.Context, sourceID string, metric float64) {
+func publishMetric(ctx context.Context, sourceID string, metric interface{}) error {
 
+	// derive typed vlaue from passed interface
+	var val *monitoringpb.TypedValue
+	switch v := metric.(type) {
+	default:
+		return fmt.Errorf("Unsuported metric type: %T", v)
+	case float64:
+		val = &monitoringpb.TypedValue{
+			Value: &monitoringpb.TypedValue_DoubleValue{DoubleValue: metric.(float64)},
+		}
+	case int64:
+		val = &monitoringpb.TypedValue{
+			Value: &monitoringpb.TypedValue_Int64Value{Int64Value: metric.(int64)},
+		}
+	}
+
+	// create data point
 	dataPoint := &monitoringpb.Point{
 		Interval: &monitoringpb.TimeInterval{
 			EndTime: &googlepb.Timestamp{Seconds: time.Now().Unix()},
 		},
-		Value: &monitoringpb.TypedValue{
-			Value: &monitoringpb.TypedValue_DoubleValue{
-				DoubleValue: metric,
-			},
-		},
+		Value: val,
 	}
 
+	// create time series request with the data point
 	tsRequest := &monitoringpb.CreateTimeSeriesRequest{
 		Name: monitoring.MetricProjectPath(projectID),
 		TimeSeries: []*monitoringpb.TimeSeries{
 			{
 				Metric: &metricpb.Metric{
-					Type:   customMetricType,
+					Type:   metricType,
 					Labels: map[string]string{"source_id": sourceID},
 				},
 				Resource: &monitoredrespb.MonitoredResource{
@@ -108,9 +106,8 @@ func publishMetric(ctx context.Context, sourceID string, metric float64) {
 		},
 	}
 
-	if err := monitorClient.CreateTimeSeries(ctx, tsRequest); err != nil {
-		log.Printf("Failed to write time series data: %v", err)
-	}
+	// publish series
+	return monitorClient.CreateTimeSeries(ctx, tsRequest)
 
 }
 
